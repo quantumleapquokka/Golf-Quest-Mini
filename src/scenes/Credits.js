7a417058ab4a82f0ced99367e3bf13a513374108
Samantha Siew <sammie.siew@gmail.com>
2025-03-11 04:37:40 -0700

    wrote credits

--- src/scenes/Credits.js
+++ src/scenes/Credits.js
@@ -10,25 +10,27 @@ class Credits extends Phaser.Scene {
         let creditsConfig = {
             fontFamily: 'times',
             fontSize: '80px',
-            color: '#513f60',
+            color: '#000000',
             backgroundColor: '',
             padding: {
                 top: 5,
                 bottom: 5,
             },
             fixedWidth: 0,
-            aligh: 'center'
+            align: 'center'
         }
 
         // display menu text
-        this.add.text(game.config.width/2, game.config.height/3 - borderUISize - borderPadding, '~CREDITS~', creditsConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/3 - borderUISize - borderPadding- 50, '★CREDITS★', creditsConfig).setOrigin(0.5)
         creditsConfig.fontSize = '35px'
-        this.add.text(game.config.width/2, game.config.height/2 - borderUISize - 90, '★彡♥~*~*~✧~*~*~♥彡★', creditsConfig).setOrigin(0.5)
-        this.add.text(game.config.width/2, game.config.height/2, ' Visual Assets(sprites, ui, etc.) && Code: Samantha Siew', creditsConfig).setOrigin(0.5)
 
-        creditsConfig.backgroundColor = '#9a6498'
-        creditsConfig.color = '#000'
-        this.add.text(game.config.width/2, game.config.height/2 + borderUISize * 3 + borderPadding * 3, 'press [SPACE] to go back to main menu', creditsConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/3, 'Code - Samantha Siew', creditsConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/2.5, 'Visual Assets(sprites, ui, etc.) - Samantha Siew', creditsConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/2.0, 'Sound Assets: made on onlinesound.net \nor found on internet copyright free - Samantha Siew', creditsConfig).setOrigin(0.5)
+        this.add.text(game.config.width/2, game.config.height/1.5, 'TV Show - Steven Universe (S1 EP19 [Rose\'s Room\])', creditsConfig).setOrigin(0.5)
+
+        creditsConfig.backgroundColor = '#40f549'
+        this.add.text(game.config.width/2, game.config.height/2 + borderUISize * 3 + borderPadding * 5, 'press [SPACE] to go back to main menu', creditsConfig).setOrigin(0.5)
         
         // define keys
         this.keySPACE = this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.SPACE)    }
