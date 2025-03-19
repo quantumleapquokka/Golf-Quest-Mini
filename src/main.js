f02e86fc41f064fc3289cd72d381ceaa92e92aea
Samantha Siew <sammie.siew@gmail.com>
2025-03-18 22:50:10 -0700

    comment

--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
 // Game Title: Golf Quest Mini
 // Name: Samantha Siew
 // Major Phaser Components: tilemaps, cameras, animation manager, physics system, tween manager
-/** */
+/**
+ * Something I added to the game, different than the original shown game is having a maximum par that the user has to score under or else they lose.
+ * Also, with the par being 7, the player can't only use the easiest golf club, so they are challenged to try two or three of them.
+ * If they only used the easiest club, their par would be over the maximum par.
+ * The meter and timed shot mechanic was also a new feature that was not in the original, so that there was a bit of challenge in the game.
+*/
 
 
 
